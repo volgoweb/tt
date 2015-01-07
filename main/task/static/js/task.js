9cb6ab7a9517e76a591774b3e656353dbe9dfe4d
volgoweb <volgoweb@bk.ru>
2015-01-07 11:29:35 +0300

    Добавил очищение формы добавления задачи после сабмита

--- main/task/static/js/task.js
+++ main/task/static/js/task.js
@@ -6,83 +6,85 @@ app.base_url = '/tasks/';
 app.base_rest_url = app.base_url + 'rest/';
 
 app.controller('tasksList', function($scope, $http) {
-  this.getTasksCollection = function() {
-      $http.get(app.base_rest_url).success(function(data) {
-        $scope.tasks = {};
-        $scope.main = main;
+    this.getTasksCollection = function() {
+        $http.get(app.base_rest_url).success(function(data) {
+            $scope.tasks = {};
+            $scope.main = main;
 
-        $.each(data, function(i, el) {
-          $scope.tasks[el.id] = el;
+            $.each(data, function(i, el) {
+                $scope.tasks[el.id] = el;
+            });
         });
-      });
-  };
+    };
 
-  this.getTasksCollection();
+    this.getTasksCollection();
 
-  $http.get(app.base_url + 'get-models/').success(function(data) {
-    $scope.models = data;
-    console.log($scope.models);
-  });
+    $http.get(app.base_url + 'get-models/').success(function(data) {
+        $scope.models = data;
+        console.log($scope.models);
+    });
 
-  $scope.new_task = {
-      priority: {
-        value: 0,
-      },
-  };
-  $scope.addTaskFormVisible = false;
+    $scope.new_task_initial = {
+        priority: {
+            value: 0,
+        },
+    };
+    $scope.new_task = angular.copy($scope.new_task_initial)
+    $scope.addTaskFormVisible = false;
 
 
-  this.showDetailTask = function(task_id) {
-    $scope.chosen_task = $scope.tasks[task_id];
-    $scope.chosen_task.view_mode = 'view';
-    console.log($scope.chosen_task);
-  };
-
-  this.setModelFieldTitle = function(task, field_name) {
-      var value = task[field_name]['value'];
-      task[field_name]['title'] = task[field_name].choices[value];
-  };
-
-  this.saveTask = function(task) {
-    var csrf = global_ajax_csrf.getCookie('csrftoken');
-    $http.defaults.headers.post['X-CSRFToken'] = csrf;
-    $http.defaults.headers.put['X-CSRFToken'] = csrf;
-    // TODO добавить для всех аякс-загрузок заставку-ожидание
-    $http.put(app.base_rest_url + task.id, task).success(function(data) {
-        $scope.tasks[task.id] = data
-        $scope.taskDetailForm.$setPristine();
+    this.showDetailTask = function(task_id) {
+        $scope.chosen_task = $scope.tasks[task_id];
         $scope.chosen_task.view_mode = 'view';
-    });
-  };
+        console.log($scope.chosen_task);
+    };
+
+    this.setModelFieldTitle = function(task, field_name) {
+        var value = task[field_name]['value'];
+        task[field_name]['title'] = task[field_name].choices[value];
+    };
+
+    this.saveTask = function(task) {
+        var csrf = global_ajax_csrf.getCookie('csrftoken');
+        $http.defaults.headers.post['X-CSRFToken'] = csrf;
+        $http.defaults.headers.put['X-CSRFToken'] = csrf;
+        // TODO добавить для всех аякс-загрузок заставку-ожидание
+        $http.put(app.base_rest_url + task.id, task).success(function(data) {
+            $scope.tasks[task.id] = data
+            $scope.taskDetailForm.$setPristine();
+            $scope.chosen_task.view_mode = 'view';
+        });
+    };
 
 
-  this.showAddTask = function() {
-      $scope.addTaskFormVisible = true;
-  };
+    this.showAddTask = function() {
+        $scope.addTaskFormVisible = true;
+    };
 
-  this.addTask = function(task) {
-    var that = this;
-    var csrf = global_ajax_csrf.getCookie('csrftoken');
-    $http.defaults.headers.post['X-CSRFToken'] = csrf;
-    $http.defaults.headers.put['X-CSRFToken'] = csrf;
-    // TODO добавить для всех аякс-загрузок заставку-ожидание
-    $http.post(app.base_rest_url, task).success(function(data) {
-        $scope.addTaskFormVisible = false;
-        // TODO разобраться, как обнулять форму
-        // $scope.taskAddForm.$setPristine();
-        $scope.taskAddForm.$dirty = false;
-        $scope.taskAddForm.$pristine = true;
-        $scope.taskAddForm.$submitted = false;
-        that.getTasksCollection();
-    });
-  };
+    this.addTask = function(task) {
+        var that = this;
+        var csrf = global_ajax_csrf.getCookie('csrftoken');
+        $http.defaults.headers.post['X-CSRFToken'] = csrf;
+        $http.defaults.headers.put['X-CSRFToken'] = csrf;
+        // TODO добавить для всех аякс-загрузок заставку-ожидание
+        $http.post(app.base_rest_url, task).success(function(data) {
+            $scope.addTaskFormVisible = false;
+            // TODO разобраться, как обнулять форму
+            $scope.taskAddForm.$setPristine();
+            // $scope.taskAddForm.$dirty = false;
+            // $scope.taskAddForm.$pristine = true;
+            // $scope.taskAddForm.$submitted = false;
+            $scope.new_task = angular.copy($scope.new_task_initial)
+            that.getTasksCollection();
+        });
+    };
 
-  this.runAction = function(task_id, action_name) {
-    $http.get(app.base_rest_url + task_id + '/run_action/?action=' + action_name).success(function(data) {
-        $scope.chosen_task = $scope.tasks[task_id] = data
-        $scope.chosen_task.view_mode = 'view';
-    });
-  };
+    this.runAction = function(task_id, action_name) {
+        $http.get(app.base_rest_url + task_id + '/run_action/?action=' + action_name).success(function(data) {
+            $scope.chosen_task = $scope.tasks[task_id] = data
+            $scope.chosen_task.view_mode = 'view';
+        });
+    };
 });
 
 // app.filter('completedTaskCheckbox', function () {
